5e151410c1091e358d24d08c8380933892a30174
Mauricio Elio Flores Herbas <mfloresherbas00@gmail.com>
2024-10-20 16:12:52 -0400

    no me deja hacer pull

--- weather-monitoring-app/src/app/page.tsx
+++ weather-monitoring-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { Button } from '@/components/ui/atoms/Button';
+import { Button } from "@/components/ui/atoms/Button";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
 export default function Home() {
@@ -18,6 +18,7 @@ export default function Home() {
         </Button>
         <ThemeSwitcher />
       </main>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
