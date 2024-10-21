c27578a376e4d019f2e3501f21636e70e426bea3
Mauricio Elio Flores Herbas <mfloresherbas00@gmail.com>
2024-10-21 00:36:58 -0400

    sacando datos de pruebade la principal

--- weather-monitoring-app/src/app/page.tsx
+++ weather-monitoring-app/src/app/page.tsx
@@ -1,25 +1,8 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/atoms/Button";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
-import { LineChartComponent } from "@/components/ui/atoms/lineChart/LineChartComponent";
-import { PositiveAndNegativeBarChart } from "@/components/ui/atoms/positiveAndNegativeBarChart/PositiveAndNegativeBarChart";
-import { SamedataComposedChart } from "@/components/ui/atoms/samedataComposedChart/SamedataComposedChart";
-import { PieChartWithNeedle } from "@/components/ui/atoms/pieChartWithNeedle/PieChartWithNeedle";
-import { ContainerBarChars } from "@/components/ui/atoms/containerBarChars/ContainerBarChars";
 
 export default function Home() {
-  const defaultData = [
-    { name: "Jan", uv: 4000, pv: 2400, amt: 2400 },
-    { name: "Feb", uv: 3000, pv: 1398, amt: 2210 },
-    { name: "Mar", uv: 2000, pv: 9800, amt: 2290 },
-  ];
-
-  const dataPie = [
-    { name: "A", value: 80, color: "#ff0000" },
-    { name: "B", value: 45, color: "#00ff00" },
-    { name: "C", value: 25, color: "#0000ff" },
-  ];
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -33,13 +16,6 @@ export default function Home() {
           Click me
         </Button>
         <ThemeSwitcher />
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4">
-          <ContainerBarChars>   <LineChartComponent data={defaultData} /></ContainerBarChars>
-          <ContainerBarChars>   <PositiveAndNegativeBarChart data={defaultData} /> </ContainerBarChars>
-          <ContainerBarChars>   <PieChartWithNeedle data={dataPie} /></ContainerBarChars>
-          <ContainerBarChars>  <SamedataComposedChart data={defaultData} /></ContainerBarChars>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
